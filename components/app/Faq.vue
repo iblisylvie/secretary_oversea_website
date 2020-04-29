028ba540b009e5390c1940c23641b50e568a222c
Runhang Zhang <jisi724@gmail.com>
2020-04-29 22:59:14 +0800

    add faq section

---
+++ components/app/Faq.vue
@@ -0,0 +1,80 @@
+<template>
+  <article @click="toggle">
+    <div class="content">
+      <h4>{{ issue.question }}</h4>
+      <p :class="{ 'is-active': active }">
+        {{ issue.answer }}
+      </p>
+    </div>
+    <i :class="{ arrow: true, 'is-active': active }"></i>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    issue: {
+      type: Object,
+      default() {
+        return {
+          question: '',
+          answer: ''
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.active = !this.active
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+article {
+  width: 100%;
+  background: $white;
+  padding: 1.5rem 3rem;
+  border-radius: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  margin-bottom: 2rem;
+
+  .content {
+    margin: 0;
+    width: 80%;
+
+    p {
+      margin: 0px;
+      max-height: 24px;
+      overflow: hidden;
+
+      &.is-active {
+        max-height: 1000rem;
+      }
+    }
+  }
+
+  .arrow {
+    align-self: self-start;
+    margin-top: 25px;
+    border: solid #59687a;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    transform: rotate(45deg);
+
+    &.is-active {
+      transform: rotate(-135deg);
+    }
+  }
+}
+</style>
--- pages/mbr-faq/index.vue
+++ pages/mbr-faq/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div></div>
+  <div v-if="issues" class="faqs">
+    <Faq v-for="issue in issues" :key="issue.question" :issue="issue" />
+  </div>
 </template>
 
 <script>
+import issues from '~/static/specific-faq.json'
+import Faq from '~/components/app/Faq.vue'
 export default {
   name: 'Account',
-  layout: 'dashboard'
+  layout: 'dashboard',
+  components: { Faq },
+  asyncData({ params }) {
+    return { issues }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.faqs {
+  padding: 4rem 3rem 0 3rem;
+  overflow: scroll;
+  height: calc(100vh - 3rem);
+}
+</style>

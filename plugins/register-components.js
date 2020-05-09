65edded6a29705e4013853f520cd05242341a047
hurley <herryfov@gmail.com>
2020-05-10 00:33:43 +0800

    feat: t-button component

--- plugins/register-components.js
+++ plugins/register-components.js
@@ -7,8 +7,10 @@ import SvgIcon from '~/components/utils/svg-icon'
 import Steps from '~/components/utils/steps'
 import Avatar from '~/components/utils/avatar'
 import Message from '~/components/utils/message'
+import Button from '~/components/utils/t-button'
 
 Vue.use(SvgIcon)
 Vue.use(Steps)
 Vue.use(Message)
 Vue.use(Avatar)
+Vue.use(Button)
